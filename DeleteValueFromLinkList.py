019f7f8b89f28dddceaef405ab775bbca6eeb4a2
Sujit Mandal <49348037+sujitmandal@users.noreply.github.com>
2019-06-29 15:37:04 +0530

    Update DeleteValueFromLinkList.py

--- DeleteValueFromLinkList.py
+++ DeleteValueFromLinkList.py
@@ -18,30 +18,34 @@ class linkList:
         print(info + ' inserted into Linked List')
 
     def deleteinfo(self,value):
-        value = input('Enete the value for delete: ')
-        if self.head is None:
-            print('\nList is empty!')
-            return
-
-        #delete first node
-        if self.head.info == value:
-            self.head = self.head.link
-            return
-
-        #delete in between or at the end
-        p = self.head
-        while p.link is not None:
-            if p.link.info == value:
-                break
-            p = p.link
-
-        if p.link is None:
-            print('\nvalue ',value + 'is not in list')
+        if self.head == None:
+            print('\nList is Empty!')
+        
         else:
-            p.link = p.link.link
+            value = input('Enete the value for delete: ')
+            if self.head is None:
+                print('\nList is empty!')
+                return
 
-        print('\nlist after deleted.')
-        self.display()
+            #delete first node
+            if self.head.info == value:
+                self.head = self.head.link
+                return
+
+            #delete in between or at the end
+            p = self.head
+            while p.link is not None:
+                if p.link.info == value:
+                    break
+                p = p.link
+
+            if p.link is None:
+                print('\nvalue ',value + 'is not in list')
+            else:
+                p.link = p.link.link
+
+            print('\nlist after deleted.')
+            self.display()
         
     def display(self):
         if self.head is None:
